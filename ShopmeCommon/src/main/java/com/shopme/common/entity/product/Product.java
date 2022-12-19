ed9cde77d61d9f805c05f155b282d1e08fa54404
KhaiHoanNB <105690960+KhaiHoanNB@users.noreply.github.com>
2022-12-19 21:20:36 +0700

    second commit

--- ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
@@ -77,12 +77,21 @@ public class Product extends IdBasedEntity {
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<ProductDetail> details = new ArrayList<>();
 
+	private int reviewCount;
+	private float averageRating;
+	
+	@Transient private boolean customerCanReview;
+	@Transient private boolean reviewedByCustomer;
 	
 	public Product(Integer id) {
 		this.id = id;
 	}
 
 	public Product() {
+	}
+	
+	public Product(String name) {
+		this.name = name;
 	}
 
 	public String getName() {
@@ -297,4 +306,43 @@ public class Product extends IdBasedEntity {
 		}
 		return this.price;
 	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+
+	public float getAverageRating() {
+		return averageRating;
+	}
+
+	public void setAverageRating(float averageRating) {
+		this.averageRating = averageRating;
+	}
+	
+	@Transient
+	public String getURI() {
+		return "/p/" + this.alias + "/";
+	}
+
+	public boolean isCustomerCanReview() {
+		return customerCanReview;
+	}
+
+	public void setCustomerCanReview(boolean customerCanReview) {
+		this.customerCanReview = customerCanReview;
+	}
+
+	public boolean isReviewedByCustomer() {
+		return reviewedByCustomer;
+	}
+
+	public void setReviewedByCustomer(boolean reviewedByCustomer) {
+		this.reviewedByCustomer = reviewedByCustomer;
+	}
+	
+	
 }
